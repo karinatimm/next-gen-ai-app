0bb527041de7324a0e498b6b2ed808480051c68f
karinatimm <kartimm.webdev@gmail.com>
2025-08-26 09:08:01 +0200

    Updated database connection utils

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const db = async () => {
-  // That means it is already connected to database.
   if (mongoose.connection.readyState >= 1) return;
-  // If not, we are going to connect await mongoose and we
-  // give the connection string
+
   await mongoose.connect(process.env.DATABASE as string);
 };
 
