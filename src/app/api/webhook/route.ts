8a3c86e6bcc9718ffffe7876d5aceb009107e97f
karinatimm <kartimm.webdev@gmail.com>
2025-08-26 09:03:16 +0200

    Updated Stripe webhook route

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,11 +1,4 @@
-// вебхук - это способ, с помощью которого Stripe уведомляет ваш сервер о
-// событиях, чтобы вы могли на них реагировать: сохранять данные, выдавать
-// доступ к продукту, отправлять email и т.д.
-// Вебхук Stripe срабатывает на сервере, и не по твоей инициативе, а когда Stripe
-// решает, что произошло событие (оплата, отмена, возврат и т.д.).
-
-// вебхуки нельзя отдавать из статических страниц (SSG), нужна динамическая страница.
-export const dynamic = "force-dynamic"; // defaults to auto-dynamic
+export const dynamic = "force-dynamic";
 import db from "@/utils/db";
 import Transaction from "../../../../models/transaction";
 import stripe from "@/utils/stripe";
@@ -19,9 +12,6 @@ const POST = async (req: Request) => {
   const body = await req.text();
 
   try {
-    // verify the webhook signature and parse the event
-    //  Проверяем подпись и создаем объект события Stripe
-    // проверяет, что запрос реально от Stripe
     const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
 
     if (event.type === "checkout.session.completed") {
@@ -29,8 +19,7 @@ const POST = async (req: Request) => {
 
       console.log("STRIPE WEBHOOK SESSION RESPONSE => ", session);
 
-      // Создаем новую транзакцию в базе данных на основе данных Stripe
-      const transaction = await new Transaction({
+      const transaction = new Transaction({
         sessionId: session.id,
         customerId: session.customer,
         invoiceId: session.invoice,
@@ -49,7 +38,7 @@ const POST = async (req: Request) => {
       });
     }
   } catch (err) {
-    console.error(err);
+    console.error("⚠️ Webhook error:", err);
     return new Response("Webhook Error", { status: 400 });
   }
 };
